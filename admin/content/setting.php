e7debd5ad1404bde13250ce67c591857227b9115
dennisismail608-star <dennisismail608>
2025-08-12 14:52:05 +0700

    slider

--- admin/content/setting.php
+++ admin/content/setting.php
@@ -1,119 +1,147 @@
-<?php 
-// jika data setting ada maka update data tersebut
-// selain itu kalo belum ada maka insert data
-if(isset($_POST['simpan'])){
-  $email = $_POST ['email'];
-  $phone = $_POST ['phone'];
-  $address = $_POST ['address'];
-  $ig = $_POST ['ig'];
-  $fb = $_POST ['fb'];
-  $twitter = $_POST ['twitter'];
-  $linkedin = $_POST ['linkedin'];
-  $querySetting = mysqli_query($koneksi, "SELECT * FROM settings LIMIT 1");
-  if(mysqli_num_rows($querySetting) > 0) {
+<?php
+// jika data setting sudah ada maka update data tersebut
+
+$querySetting = mysqli_query($koneksi, "SELECT * FROM settings LIMIT 1");
+$row = mysqli_fetch_assoc($querySetting);
+
+if (isset($_POST['simpan'])) {
+  $email = $_POST['email'];
+  $phone = $_POST['phone'];
+  $address = $_POST['address'];
+  $ig = $_POST['ig'];
+  $fb = $_POST['fb'];
+  $twitter = $_POST['twitter'];
+  $linkedin = $_POST['linkedin'];
+
+  // jika gambar terupload
+  if (!empty($_FILES['logo']['name'])) {
+    $logo = $_FILES['logo']['name'];
+    $path = "uploads/";
+
+    if (!is_dir($path)) mkdir($path);
+
+    $logo_name = md5($logo);
+    $target_files = $path . $logo_name;
+
+    if (move_uploaded_file($_FILES['logo']['tmp_name'], $target_files)) {
+      // jika gambarnya ada maka gambar sebelumnya akan diganti oleh gambar baru
+      if (!empty($row['logo'])) {
+        unlink($path . $row['logo']);
+      }
+    }
+  }
+
+  if ($row) {
     // update
-    $row = mysqli_fetch_assoc($querySetting);
     $id_setting = $row['id'];
 
-    $update = mysqli_query($koneksi, "UPDATE settings SET email='$email', phone='$phone', addres='$address', ig='$ig', fb='$fb', twitter='$twitter', linkedin='$linkedin' WHERE id='$id_setting'");
+    $update = mysqli_query($koneksi, "UPDATE settings SET email = '$email', phone = '$phone', address = '$address', ig = '$ig', fb = '$fb', twitter = '$twitter', linkedin = '$linkedin', logo = '$logo_name' WHERE id = '$id_setting'");
+    if ($update) {
+      header("location:?page=setting&ubah=berhasil");
+    }
   } else {
-    // insert
-    $insert = mysqli_query($koneksi, "INSERT INTO settings (email, phone, address, ig. fb. twitter, linkedin) VALUES ('$email', '$phone', '$address', '$ig', '$fb', '$twitter', $linkedin)");
+    $insert = mysqli_query($koneksi, "INSERT INTO setting (email, phone, address, ig, fb, twitter, linkedin, logo) VALUES ('$email', '$phone', '$address', 'ig', 'fb', 'twitter', 'linkedin', 'logo')");
     if ($insert) {
-    header("location:?page=setting&tambah=berhasil");
+      header("location:?page=setting&tambah=berhasil");
     }
   }
 }
- $querySetting = mysqli_query($koneksi, "SELECT * FROM settings LIMIT 1");
- $row = mysqli_fetch_assoc($querySetting);
-?>
-<div class="pagetitle">
-      <h1>pengaturan</h1>
-    </div><!-- End Page Title -->
 
-    <section class="section">
-        <div class="row">
-           <div class="col-lg-12">
-   
-             <div class="card">
-               <div class="card-body">
-                <div class="card-tittle fw-bold">pengaturan</div>
-                 <form action="" method="post" enctype="multipart/form-data">
-                  <div class="mb-3 row">
-                    <div class="col-sm-2">
-                      <label for="" class="fw-bold">email</label>
-                    </div>
-                    <div class="col-sm-6">
-                      <input type="email" name="email" id="" class="form-control">
-                    </div>
-                  </div>
-                   <div class="mb-3 row">
-                    <div class="col-sm-2">
-                      <label for="" class="fw-bold">no telp</label>
-                    </div>
-                    <div class="col-sm-6">
-                      <input type="number" name="phone" id="" class="form-control value="<?php echo isset($row['email']) ? $row['email'] : ''?>>
-                    </div>
-                  </div>
-                  <div class="mb-3 row">
-                    <div class="col-sm-2">
-                      <label for="" class="fw-bold">Alamat</label>
-                    </div>
-                    <div class="col-sm-6">
-                      <textarea name="addres" id="" class="form-control value="<?php echo isset($row['address']) ? $row['address'] : ''?>" ></textarea>
-                    </div>
-                  </div>
-                  <div class="mb-3 row">
-                    <div class="col-sm-2">
-                      <label for="" class="fw-bold">twitter</label>
-                    </div>
-                    <div class="col-sm-6">
-                      <input type="url" name="twitter" id="" class="form-control" value="<?php echo isset($row['twitter']) ? $row['twitter'] : ''?>>
-                    </div>
-                  </div>
-                  <div class="mb-3 row">
-                    <div class="col-sm-2">
-                      <label for="" class="fw-bold">fb</label>
-                    </div>
-                    <div class="col-sm-6">
-                      <input type="url" name="fb" id="" class="form-control" value="<?php echo isset($row['fb']) ? $row['fb'] : ''?>>
-                    </div>
-                  </div>
-                  <div class="mb-3 row">
-                    <div class="col-sm-2">
-                      <label for="" class="fw-bold">ig</label>
-                    </div>
-                    <div class="col-sm-6">
-                      <input type="url" name="ig" id="" class="form-control" value="<?php echo isset($row['ig']) ? $row['ig'] : ''?>>
-                    </div>
-                  </div>
-                  <div class="mb-3 row">
-                    <div class="col-sm-2">
-                      <label for="" class="fw-bold">linkedin</label>
-                    </div>
-                    <div class="col-sm-6">
-                      <input type="url" name="linkedin" id="" class="form-control" value="<?php echo isset($row['linkedin']) ? $row['linkedin'] : ''?>>
-                    </div>
-                  </div>
-                    <div class="mb-3 row">
-                    <div class="col-sm-2">
-                      <label for="" class="fw-bold">logo</label>
-                    </div>
-                    <div class="col-sm-6">
-                      <input type="file" name="logo" id="" class="form-control">
-                    </div>
-                    
-                  </div>
-                  <div class="mb-3 row">
-                    <div class="col-sm-12">
-                      <button class="btn btn-primary" name="simpan">simpan</button>
-                    </div>
-                  </div>
-                 </form>
-               </div>
-             </div>
-   
-           </div>
-         </div>
-        
-        </section>
+?>
+
+<div class="pagetitle">
+  <h1>Setting</h1>
+</div><!-- End Page Title -->
+
+<section class="section">
+  <div class="row">
+    <div class="col-lg-12">
+
+      <div class="card">
+        <div class="card-body">
+          <h5 class="card-title">Setting</h5>
+
+
+          <form action="" method="post" enctype="multipart/form-data">
+            <div class="row mb-3">
+              <div class="col-sm-2">
+                <label for="" class="form-label fw-bold">Email</label>
+              </div>
+              <div class="col-sm-10">
+                <input type="email" name="email" id="email" class="form-control" placeholder="Masukkan Email Anda" value="<?php echo isset($row['email']) ? $row['email'] : '' ?>" autofocus>
+              </div>
+            </div>
+            <div class="row mb-3">
+              <div class="col-sm-2">
+                <label for="" class="form-label fw-bold">Phone</label>
+              </div>
+              <div class="col-sm-10">
+                <input type="number" name="phone" id="phone" class="form-control" placeholder="Masukkan telepon Anda" value="<?php echo isset($row['phone']) ? $row['phone'] : '' ?>">
+              </div>
+            </div>
+            <div class="row mb-3">
+              <div class="col-sm-2">
+                <label for="" class="form-label fw-bold">address</label>
+              </div>
+              <div class="col-sm-10">
+                <textarea name="address" class="form-control" placeholder="Masukkan address Anda" id="floatingTextarea2" style="height: 100px"><?php echo isset($row['address']) ? $row['address'] : '' ?></textarea>
+              </div>
+            </div>
+            <div class="row mb-3">
+              <div class="col-sm-2">
+                <label for="" class="form-label fw-bold">ig</label>
+              </div>
+              <div class="col-sm-10">
+                <input type="url" name="ig" id="ig" class="form-control" placeholder="Masukkan ig Anda" value="<?php echo isset($row['ig']) ? $row['ig'] : '' ?>">
+              </div>
+            </div>
+            <div class="row mb-3">
+              <div class="col-sm-2">
+                <label for="" class="form-label fw-bold">fb</label>
+              </div>
+              <div class="col-sm-10">
+                <input type="url" name="fb" id="fb" class="form-control" placeholder="Masukkan fb Anda" value="<?php echo isset($row['fb']) ? $row['fb'] : '' ?>">
+              </div>
+            </div>
+            <div class="row mb-3">
+              <div class="col-sm-2">
+                <label for="" class="form-label fw-bold">Twitter</label>
+              </div>
+              <div class="col-sm-10">
+                <input type="url" name="twitter" id="twitter" class="form-control" placeholder="Masukkan Twitter Anda" value="<?php echo isset($row['twitter']) ? $row['twitter'] : '' ?>">
+              </div>
+            </div>
+            <div class="row mb-3">
+              <div class="col-sm-2">
+                <label for="" class="form-label fw-bold">Linkedin</label>
+              </div>
+              <div class="col-sm-10">
+                <input type="url" name="linkedin" id="linkedin" class="form-control" placeholder="Masukkan Linkedin Anda" value="<?php echo isset($row['linkedin']) ? $row['linkedin'] : '' ?>">
+              </div>
+            </div>
+            <div class="row mb-3">
+              <div class="col-sm-2">
+                <label for="" class="form-label fw-bold">logo</label>
+              </div>
+              <div class="col-sm-10">
+                <input type="file" class="form-control" name="logo" id="inputGroupFile02">
+                <img class="mt-2" src="uploads/<?php echo isset($row['logo']) ? $row['logo'] : '' ?>" alt="" width="100">
+              </div>
+            </div>
+
+            <div class="row mb-3">
+              <div class="col-sm-2"></div>
+              <div class="col-sm-10">
+                <button class="btn btn-primary" type="submit" name="simpan">Simpan</button>
+                <a href="?page=user" class="btn btn-secondary" onclick="history.back()">
+                  ← Kembali
+                </a>
+              </div>
+            </div>
+          </form>
+        </div>
+      </div>
+
+    </div>
+  </div>
+</section>
---
+++ admin/content/tambah-slider.php
@@ -0,0 +1,113 @@
+<?php
+
+$id = isset($_GET['id']) ? $_GET['edit'] : '';
+
+if (isset($_GET['edit'])) {
+    $id = $_GET['edit'];
+    $query = mysqli_query($koneksi, "SELECT * FROM sliders WHERE id = '$id'");
+    $rowEdit = mysqli_fetch_assoc($query);
+
+    $title = "Edit Slider";
+} else {
+    $title = "Tambah Slider";
+}
+
+if (isset($_GET['delete'])) {
+    $id = $_GET['delete'];
+    $query = mysqli_query($koneksi, "DELETE FROM sliders WHERE id = '$id'");
+
+    header("location:?page=slider&hapus=berhasil");
+}
+
+if (isset($_POST['simpan'])) {
+    $title = $_POST['title'];
+    $description = $_POST['description'];
+
+    // jika gambar terupload
+    if (!empty($_FILES['image']['name'])) {
+        $image = $_FILES['image']['name'];
+        $tmp_name = $_FILES['image']['tmp_name'];
+        $type = mime_content_type($tmp_name);
+
+        $ext_allowed = ["image/png", "image/jpg", "image/jpeg"];
+
+        if (!in_array($type, $ext_allowed)) {
+            echo "Extensi file tidak diizinkan";
+            exit;
+        }
+
+        $path = "uploads/";
+        if (!is_dir($path)) mkdir($path);
+
+        $image_name = md5($image) . "." . pathinfo($image, PATHINFO_EXTENSION);
+        $target_files = $path . $image_name;
+
+
+        if (move_uploaded_file($tmp_name, $target_files)) {
+            echo "Upload berhasil";
+        } else {
+            echo "Upload gagal";
+        }
+    }
+
+    if ($id) {
+        // ini query update
+        $update = mysqli_query($koneksi, "UPDATE sliders SET title = '$title', description = '$description', image = '$image_name' WHERE id = '$id'");
+        if ($update) {
+            header("location:?page=slider&ubah=berhasil");
+        }
+    } else {
+        // print_r($title); 
+        // die;
+        $insert = mysqli_query($koneksi, "INSERT INTO sliders (title, description, image) VALUES ('$title', '$description', '$image')");
+        if ($insert) {
+            header("location:?page=slider&tambah=berhasil");
+        }
+    }
+}
+
+
+?>
+<div class="pagetitle">
+    <h1><?php echo $title ?></h1>
+</div><!-- End Page Title -->
+
+<section class="section">
+    <div class="row">
+        <div class="col-lg-12">
+
+            <div class="card">
+                <div class="card-body">
+                    <h5 class="card-title"><?php echo $title ?></h5>
+                    <form action="" method="post" enctype="multipart/form-data">
+                        <div class="mb-3">
+                            <label for="">Gambar</label>
+                            <input type="file" class="form-control"
+                                name="image" required>
+                            <small class="">)* Size : 1920 * 1088</small>
+                        </div>
+                        <div class="mb-3">
+                            <label for="">Judul</label>
+                            <input type="text" class="form-control"
+                                name="title"
+                                required value="<?php echo ($id) ? $rowEdit['title'] : '' ?>">
+                        </div>
+                        <div class="mb-3">
+                            <label for="">Deskripsi</label>
+                            <textarea class="form-control" name="description" required><?php echo ($id) ? $rowEdit['description'] : '' ?></textarea>
+                        </div>
+                        <div class="mb-3">
+                            <button class="btn btn-primary" type="submit" name="simpan">Simpan</button>
+                            <a href="?page=user" class="text-muted">Kembali</a>
+                        </div>
+                    </form>
+
+                </div>
+            </div>
+
+        </div>
+
+
+    </div>
+
+</section>
